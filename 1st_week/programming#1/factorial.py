c13a47030054119c46679cfd9f367acddbb70d74
Lee <walya2919@gmail.com>
2025-03-04 14:21:50 +0900

    리커젼 제한 완화 및 소요시간 출력

--- 1st_week/programming#1/factorial.py
+++ 1st_week/programming#1/factorial.py
@@ -1,4 +1,8 @@
-class InputError(Exception):
+import sys
+import time
+sys.setrecursionlimit(10002)
+
+class InputValueError(Exception):
     def __str__(self):
         return '입력값이 정수가 아닙니다.'
 
@@ -10,9 +14,15 @@ def factorial(n:int) -> int:
 
 user_input = input('숫자를 입력해 주세요: ')
 if not user_input.isdigit():
-    raise InputError
+    raise InputValueError
 
 user_input = int(user_input)
+start = time.time()
 res = factorial(user_input)
+end = time.time()
 
-print('\n입력 값에 대한 계승값: {}'.format(str(res)))
+try:
+    print(f'\n입력 값에 대한 계승값: {res}')
+except:
+    print('출력값 과다로 출력이 제한되었습니다.')
+print(f'걸린 시간: {end-start:.5f}초')
